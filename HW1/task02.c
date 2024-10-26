8c46c7ea11bb1df9e66a5e2e846eb07e0b4608fc
Pavel Shvec <pavelW437@yandex.ru>
2024-10-26 16:00:57 +0300

    first commit

---
+++ HW1/task02.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf( "Lets\n"
+			"   go\n"
+			"      to wolk\n");
+	return 0;
+}
+
---
+++ HW1/task03.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf( "   *\n"
+			"  ***\n"
+			" *****\n"
+			"*******\n"
+			" HHOHH\n"
+			" ZZZZZ\n");
+	return 0;
+}
+
